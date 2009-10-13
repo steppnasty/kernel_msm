c6213caff7b99e7bc766d1ef11224d792af13b1a
Noboru Iwamatsu <n_iwamatsu@jp.fujitsu.com>
2009-10-13 17:22:29 -0400

    xenbus: prevent warnings on unhandled enumeration values
    XenbusStateReconfiguring/XenbusStateReconfigured were introduced by
    c/s 437, but aren't handled in many switch statements.
    
    .. also pulled from the linux-2.6-sparse-tree tree.
    
    Signed-off-by: Jan Beulich <jbeulich@novell.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- drivers/block/xen-blkfront.c
+++ drivers/block/xen-blkfront.c
@@ -1105,6 +1105,8 @@ static void blkback_changed(struct xenbus_device *dev,
 	case XenbusStateInitialising:
 	case XenbusStateInitWait:
 	case XenbusStateInitialised:
+	case XenbusStateReconfiguring:
+	case XenbusStateReconfigured:
 	case XenbusStateUnknown:
 	case XenbusStateClosed:
 		break;
