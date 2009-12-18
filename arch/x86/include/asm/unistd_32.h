d509d75d3d55f86bbca6ecab765dd48ebf4c233e
Eric Paris <eparis@redhat.com>
2009-12-17 21:24:25 -0500

    fanotify: fanotify_init syscall declaration
    This patch defines a new syscall fanotify_init() of the form:
    
    int sys_fanotify_init(unsigned int flags, unsigned int event_f_flags,
    		      unsigned int priority)
    
    This syscall is used to create and fanotify group.  This is very similar to
    the inotify_init() syscall.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- arch/x86/include/asm/unistd_32.h
+++ arch/x86/include/asm/unistd_32.h
@@ -343,11 +343,12 @@
 #define __NR_rt_tgsigqueueinfo	335
 #define __NR_perf_event_open	336
 #define __NR_recvmmsg		337
-#define __NR_prlimit64		338
+#define __NR_fanotify_init	338
+#define __NR_prlimit64		339
 
 #ifdef __KERNEL__
 
-#define NR_syscalls 339
+#define NR_syscalls 340
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
