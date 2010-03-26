5c1ab3cd4890dbb466f1e3f3da1f51249341b5de
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2010-03-26 15:37:50 -0700

    xen/mmu: inhibit vmap aliases rather than trying to clear them out
    Rather than trying to deal with aliases once they appear, just completely
    inhibit them.  Mostly the removal of aliases was managable, but it comes
    unstuck in xen_create_contiguous_region() because it gets executed at
    interrupt time (as a result of dma_alloc_coherent()), which causes all
    sorts of confusion in the vmap code, as it was never intended to be run
    in interrupt context.
    
    This has the unfortunate side effect of removing all the unmap batching
    the vmap code so carefully added, but that can't be helped.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- arch/x86/xen/mmu.c
+++ arch/x86/xen/mmu.c
@@ -42,6 +42,7 @@
 #include <linux/highmem.h>
 #include <linux/debugfs.h>
 #include <linux/bug.h>
+#include <linux/vmalloc.h>
 #include <linux/module.h>
 #include <linux/gfp.h>
 
@@ -1016,8 +1017,6 @@ static int xen_pin_page(struct mm_struct *mm, struct page *page,
    read-only, and can be pinned. */
 static void __xen_pgd_pin(struct mm_struct *mm, pgd_t *pgd)
 {
-	vm_unmap_aliases();
-
 	xen_mc_batch();
 
 	if (__xen_pgd_walk(mm, pgd, xen_pin_page, USER_LIMIT)) {
@@ -1581,7 +1580,6 @@ static void xen_alloc_ptpage(struct mm_struct *mm, unsigned long pfn, unsigned l
 	if (PagePinned(virt_to_page(mm->pgd))) {
 		SetPagePinned(page);
 
-		vm_unmap_aliases();
 		if (!PageHighMem(page)) {
 			make_lowmem_page_readonly(__va(PFN_PHYS((unsigned long)pfn)));
 			if (level == PT_PTE && USE_SPLIT_PTLOCKS)
@@ -2035,6 +2033,8 @@ void __init xen_init_mmu_ops(void)
 	x86_init.paging.pagetable_setup_start = xen_pagetable_setup_start;
 	x86_init.paging.pagetable_setup_done = xen_pagetable_setup_done;
 	pv_mmu_ops = xen_mmu_ops;
+
+	vmap_lazy_unmap = false;
 }
 
 static void xen_hvm_exit_mmap(struct mm_struct *mm)
@@ -2206,8 +2206,6 @@ int xen_create_contiguous_region(unsigned long vstart, unsigned int order,
 
 	memset((void *) vstart, 0, PAGE_SIZE << order);
 
-	vm_unmap_aliases();
-
 	spin_lock_irqsave(&xen_reservation_lock, flags);
 
 	/* 1. Zap current PTEs, remembering MFNs. */
@@ -2244,8 +2242,6 @@ void xen_destroy_contiguous_region(unsigned long vstart, unsigned int order)
 		return;
 
 	memset((void *) vstart, 0, PAGE_SIZE << order);
-
-	vm_unmap_aliases();
 
 	spin_lock_irqsave(&xen_reservation_lock, flags);
 
