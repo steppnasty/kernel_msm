4b083d15a560bd4ed06327fa640146dff6242ee5
Felix Fietkau <nbd@openwrt.org>
2010-06-01 21:33:13 +0200

    ath9k: fix queue stop/start based on the number of pending frames
    Because there is a limited number of tx buffers available, once the
    queue has been filled to a certain point, ath9k needs to stop accepting
    new frames from mac80211. In order to prevent a full WMM queue from
    stopping another queue with fewer frames, this patch limits the number
    of queued frames to a quarter of the total available tx buffers, minus
    some reserved frames to be used for other purposes (e.g. beacons).
    
    Because tx buffers are reserved for frames when they're staged in
    software queues as well, the actual queue depth cannot be used for
    this, so this patch stores a reference to the tx queue in the ath_buf
    struct and keeps track of the total number of pending frames.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -1035,6 +1035,7 @@ static int ath9k_tx(struct ieee80211_hw *hw,
 	struct ath_tx_control txctl;
 	int padpos, padsize;
 	struct ieee80211_hdr *hdr = (struct ieee80211_hdr *) skb->data;
+	int qnum;
 
 	if (aphy->state != ATH_WIPHY_ACTIVE && aphy->state != ATH_WIPHY_SCAN) {
 		ath_print(common, ATH_DBG_XMIT,
@@ -1107,11 +1108,8 @@ static int ath9k_tx(struct ieee80211_hw *hw,
 		memmove(skb->data, skb->data + padsize, padpos);
 	}
 
-	/* Check if a tx queue is available */
-
-	txctl.txq = ath_test_get_txq(sc, skb);
-	if (!txctl.txq)
-		goto exit;
+	qnum = ath_get_hal_qnum(skb_get_queue_mapping(skb), sc);
+	txctl.txq = &sc->tx.txq[qnum];
 
 	ath_print(common, ATH_DBG_XMIT, "transmitting packet, skb: %p\n", skb);
 
