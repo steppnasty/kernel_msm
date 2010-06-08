b5879b36e7f1bfa023bda39849cf15e0f8ba8cb6
Grant Likely <grant.likely@secretlab.ca>
2010-06-08 07:48:09 -0600

    of/address: merge of_iomap()
    Merge common code between Microblaze and PowerPC.  This patch creates
    new of_address.h and address.c files to containing address translation
    and mapping routines.  First routine to be moved it of_iomap()
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    CC: Michal Simek <monstr@monstr.eu>
    CC: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/include/asm/prom.h
+++ arch/powerpc/include/asm/prom.h
@@ -18,6 +18,7 @@
  */
 #include <linux/types.h>
 #include <linux/of_fdt.h>
+#include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/proc_fs.h>
 #include <linux/platform_device.h>
@@ -125,15 +126,6 @@ static inline int of_node_to_nid(struct device_node *device) { return 0; }
  */
 struct pci_dev;
 extern int of_irq_map_pci(struct pci_dev *pdev, struct of_irq *out_irq);
-
-/**
- * of_iomap - Maps the memory mapped IO for a given device_node
- * @device:	the device whose io range will be mapped
- * @index:	index of the io range
- *
- * Returns a pointer to the mapped memory
- */
-extern void __iomem *of_iomap(struct device_node *device, int index);
 
 #endif /* __KERNEL__ */
 #endif /* _POWERPC_PROM_H */
