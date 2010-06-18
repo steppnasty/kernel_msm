d3586a19dafec768e6e6a2bc9576fed39f1fc7db
Grant Likely <grant.likely@secretlab.ca>
2010-06-18 11:10:01 -0600

    of/powerpc: Move Powermac irq quirk code into powermac pic driver code
    The code that figures out what is wrong with the powermac irq device
    tree data belongs with the rest of the powermac irq code.  This patch
    moves it out of prom_parse.c and into powermac/pic.c so that it is only
    compiled in when actually needed.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/include/asm/prom.h
+++ arch/powerpc/include/asm/prom.h
@@ -116,18 +116,24 @@ static inline int of_node_to_nid(struct device_node *device) { return 0; }
  * OF interrupt mapping
  */
 
-/**
- * of_irq_map_init - Initialize the irq remapper
- * @flags:	flags defining workarounds to enable
- *
- * Some machines have bugs in the device-tree which require certain workarounds
- * to be applied. Call this before any interrupt mapping attempts to enable
- * those workarounds.
- */
 #define OF_IMAP_OLDWORLD_MAC	0x00000001
 #define OF_IMAP_NO_PHANDLE	0x00000002
 
-extern void of_irq_map_init(unsigned int flags);
+#if defined(CONFIG_PPC32) && defined(CONFIG_PPC_PMAC)
+/* Workarounds only needed for 32bit powermac machines */
+extern unsigned int of_irq_workarounds;
+extern struct device_node *of_irq_dflt_pic;
+extern int of_irq_map_oldworld(struct device_node *device, int index,
+			       struct of_irq *out_irq);
+#else
+#define of_irq_workarounds (0)
+#define of_irq_dflt_pic (NULL)
+static inline int of_irq_map_oldworld(struct device_node *device, int index,
+				      struct of_irq *out_irq)
+{
+	return -EINVAL;
+}
+#endif
 
 /**
  * of_irq_map_raw - Low level interrupt tree parsing
