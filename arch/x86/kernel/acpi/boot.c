f4eb83f8e03b4b95d7f41d17bbe9d65c22884b49
Jeremy Fitzhardinge <jeremy@goop.org>
2010-06-24 17:05:41 +0100

    xen: add xen hvm acpi_register_gsi variant
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Acked-by: Rafael J. Wysocki <rjw@sisk.pl>

--- arch/x86/kernel/acpi/boot.c
+++ arch/x86/kernel/acpi/boot.c
@@ -543,7 +543,8 @@ static int acpi_register_gsi_ioapic(struct device *dev, u32 gsi,
 	return gsi;
 }
 
-static int (*__acpi_register_gsi)(struct device *dev, u32 gsi, int trigger, int polarity) = acpi_register_gsi_pic;
+int (*__acpi_register_gsi)(struct device *dev, u32 gsi,
+			   int trigger, int polarity) = acpi_register_gsi_pic;
 
 /*
  * success: return IRQ number (>=0)
