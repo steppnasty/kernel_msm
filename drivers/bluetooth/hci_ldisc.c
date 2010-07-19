8223ac839f58664b49f7b506e71debdf09cd367f
Suraj Sumangala <suraj@atheros.com>
2010-07-19 12:34:07 +0530

    Bluetooth: Support for Atheros AR300x serial chip
    Implements Atheros AR300x serial HCI protocol.
    
    This protocol extends H4 serial protocol to implement enhanced power
    management features supported by Atheros AR300x serial Bluetooth chipsets.
    
    Signed-off-by: Suraj Sumangala <suraj@atheros.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_ldisc.c
+++ drivers/bluetooth/hci_ldisc.c
@@ -559,6 +559,9 @@ static int __init hci_uart_init(void)
 #ifdef CONFIG_BT_HCIUART_LL
 	ll_init();
 #endif
+#ifdef CONFIG_BT_HCIUART_ATH3K
+	ath_init();
+#endif
 
 	return 0;
 }
@@ -575,6 +578,9 @@ static void __exit hci_uart_exit(void)
 #endif
 #ifdef CONFIG_BT_HCIUART_LL
 	ll_deinit();
+#endif
+#ifdef CONFIG_BT_HCIUART_ATH3K
+	ath_deinit();
 #endif
 
 	/* Release tty registration of line discipline */
