cd9152342e371c10044a4f9fb41892ec9b71e27c
Al Viro <viro@zeniv.linux.org.uk>
2010-07-25 00:17:56 +0400

    beginning of transtion: ->mount()
    eventual replacement for ->get_sb() - does *not* get vfsmount,
    return ERR_PTR(error) or root of subtree to be mounted.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1773,6 +1773,8 @@ struct file_system_type {
 	int fs_flags;
 	int (*get_sb) (struct file_system_type *, int,
 		       const char *, void *, struct vfsmount *);
+	struct dentry *(*mount) (struct file_system_type *, int,
+		       const char *, void *);
 	void (*kill_sb) (struct super_block *);
 	struct module *owner;
 	struct file_system_type * next;
