e58fbb0b9d7214555e280a8c4cc0664a63f2d840
Heikki Krogerus <ext-heikki.krogerus@nokia.com>
2010-10-04 10:51:37 +0300

    power_supply: Add types for USB chargers
    This adds power supply types for USB chargers defined in
    Battery Charging Specification 1.1.
    
    Signed-off-by: Heikki Krogerus <ext-heikki.krogerus@nokia.com>
    Signed-off-by: Anton Vorontsov <cbouatmailru@gmail.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -43,6 +43,7 @@ static ssize_t power_supply_show_property(struct device *dev,
 					  char *buf) {
 	static char *type_text[] = {
 		"Battery", "UPS", "Mains", "USB", "Wireless"
+		"USB_DCP", "USB_CDP", "USB_ACA"
 	};
 	static char *status_text[] = {
 		"Unknown", "Charging", "Discharging", "Not charging", "Full"
