3e4eea39125230d160ff8fbc6f96adaf1aa97ad8
Namhyung Kim <namhyung@gmail.com>
2010-10-27 21:30:11 -0400

    ext4: Check return value of sb_getblk() and friends
    Fail block allocation if sb_getblk() returns NULL. In that case,
    sb_find_get_block() also likely to fail so that it should skip
    calling ext4_forget().
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -4540,6 +4540,8 @@ void ext4_free_blocks(handle_t *handle, struct inode *inode,
 			if (!bh)
 				tbh = sb_find_get_block(inode->i_sb,
 							block + i);
+			if (unlikely(!tbh))
+				continue;
 			ext4_forget(handle, flags & EXT4_FREE_BLOCKS_METADATA,
 				    inode, tbh, block + i);
 		}
