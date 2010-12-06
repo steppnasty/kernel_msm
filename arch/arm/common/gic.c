b673383ebcaf7d23d070e591cc0c5b4e43884b0d
Russell King <rmk+kernel@arm.linux.org.uk>
2010-12-06 10:38:14 +0000

    ARM: GIC: move enablement of PPI interrupts to gic.c
    Avoid adding nasty genirq-specific code to local timers to enable PPI
    interrupts.  Instead, provide a gic function to do this.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -456,6 +456,16 @@ void __cpuinit gic_cpu_init(unsigned int gic_nr, void __iomem *base)
 	mb();
 }
 
+void __cpuinit gic_enable_ppi(unsigned int irq)
+{
+	unsigned long flags;
+
+	local_irq_save(flags);
+	irq_to_desc(irq)->status |= IRQ_NOPROBE;
+	gic_unmask_irq(irq);
+	local_irq_restore(flags);
+}
+
 #ifdef CONFIG_SMP
 void gic_raise_softirq(const struct cpumask *mask, unsigned int irq)
 {
