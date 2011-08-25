868c5394ed26be8cd1ffa0783a1de251b33ca88e
Jean Pihet <j-pihet@ti.com>
2011-08-25 15:35:12 +0200

    PM QoS: Minor clean-ups
     - Misc fixes to improve code readability:
      * rename struct pm_qos_request_list to struct pm_qos_request,
      * rename pm_qos_req parameter to req in internal code,
        consistenly use req in the API parameters,
      * update the in-kernel API callers to the new parameters names,
      * rename of fields names (requests, list, node, constraints)
    
    Signed-off-by: Jean Pihet <j-pihet@ti.com>
    Acked-by: markgross <markgross@thegnar.org>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- include/sound/pcm.h
+++ include/sound/pcm.h
@@ -368,7 +368,7 @@ struct snd_pcm_substream {
 	int stream;			/* stream (direction) */
 	char latency_id[20];		/* latency identifier */
 	
-	struct pm_qos_request_list latency_pm_qos_req; /* pm_qos request */
+	struct pm_qos_request latency_pm_qos_req; /* pm_qos request */
 	size_t buffer_bytes_max;	/* limit ring buffer size */
 	struct snd_dma_buffer dma_buffer;
 	unsigned int dma_buf_id;
