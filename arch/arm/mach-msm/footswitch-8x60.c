505586daa957b1508a9357bbe471022a782b7483
Stephen Boyd <sboyd@codeaurora.org>
2011-12-28 19:15:57 -0800

    msm: footswitch-8x60: Reverse order of reset assertion
    The order in which a power domain is put into reset matters for
    ijpeg. Revert back to the previous order of resets by looping
    over the clock array backwards. This should maintain the reset
    order we had before, i.e. axi, ahb, and then core clocks.
    
    CRs-Fixed: 327801
    Change-Id: Ie228615a54a46cc0e408f2c646bcb979c30e59ff
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/footswitch-8x60.c
+++ arch/arm/mach-msm/footswitch-8x60.c
@@ -175,6 +175,8 @@ static int footswitch_enable(struct regulator_dev *rdev)
 	 * and resets should be asserted before power is restored.
 	 */
 	for (clock = fs->clk_data; clock->clk; clock++)
+		; /* Do nothing */
+	for (clock--; clock >= fs->clk_data; clock--)
 		clk_reset(clock->clk, CLK_RESET_ASSERT);
 	/* Wait for synchronous resets to propagate. */
 	udelay(RESET_DELAY_US);
@@ -251,6 +253,8 @@ static int footswitch_disable(struct regulator_dev *rdev)
 	 * outputs settle prior to clamping.
 	 */
 	for (clock = fs->clk_data; clock->clk; clock++)
+		; /* Do nothing */
+	for (clock--; clock >= fs->clk_data; clock--)
 		clk_reset(clock->clk, CLK_RESET_ASSERT);
 	/* Wait for synchronous resets to propagate. */
 	udelay(RESET_DELAY_US);
@@ -322,6 +326,8 @@ static int gfx2d_footswitch_enable(struct regulator_dev *rdev)
 	 * and resets should be asserted before power is restored.
 	 */
 	for (clock = fs->clk_data; clock->clk; clock++)
+		; /* Do nothing */
+	for (clock--; clock >= fs->clk_data; clock--)
 		clk_reset(clock->clk, CLK_RESET_ASSERT);
 	/* Wait for synchronous resets to propagate. */
 	udelay(RESET_DELAY_US);
@@ -388,6 +394,8 @@ static int gfx2d_footswitch_disable(struct regulator_dev *rdev)
 	 * outputs settle prior to clamping.
 	 */
 	for (clock = fs->clk_data; clock->clk; clock++)
+		; /* Do nothing */
+	for (clock--; clock >= fs->clk_data; clock--)
 		clk_reset(clock->clk, CLK_RESET_ASSERT);
 	/* Wait for synchronous resets to propagate. */
 	udelay(5);
