c4e49510185b1a014ae85792fd9af4b660b416bd
Grant Likely <grant.likely@secretlab.ca>
2012-02-15 20:38:40 -0700

    of: Only compile OF_DYNAMIC on PowerPC pseries and iseries
    Only two architectures use the OF node reference counting and reclaim bits.
    There is no need to compile it for the rest of the PowerPC platforms or for
    any of the other architectures.  This patch makes iseries and pseries
    select CONFIG_OF_DYNAMIC, and makes it default to off for everything else.
    
    It is still safe to turn on CONFIG_OF_DYNAMIC on all architectures, it just
    isn't necessary.
    
    v2: Also select OF_DYNAMIC for PPC_CHROMA and MPC885ADS as reported by Michael
        Meuling
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: David S. Miller <davem@davemloft.net>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: Jimi Xenidis <jimix@pobox.com> (for PPC_CHROMA bug fix)
    Cc: Rob Herring <rob.herring@calxeda.com>

--- include/linux/of.h
+++ include/linux/of.h
@@ -65,26 +65,24 @@ struct device_node {
 #endif
 };
 
-#if defined(CONFIG_SPARC) || !defined(CONFIG_OF)
-/* Dummy ref counting routines - to be implemented later */
-static inline struct device_node *of_node_get(struct device_node *node)
-{
-	return node;
-}
-static inline void of_node_put(struct device_node *node)
-{
-}
-#else
-extern struct device_node *of_node_get(struct device_node *node);
-extern void of_node_put(struct device_node *node);
-#endif
-
-#define MAX_PHANDLE_ARGS 8
+define MAX_PHANDLE_ARGS 8
 struct of_phandle_args {
 	struct device_node *np;
 	int args_count;
 	uint32_t args[MAX_PHANDLE_ARGS];
 };
+
+#ifdef CONFIG_OF_DYNAMIC
+extern struct device_node *of_node_get(struct device_node *node);
+extern void of_node_put(struct device_node *node);
+#else /* CONFIG_OF_DYNAMIC */
+/* Dummy ref counting routines - to be implemented later */
+static inline struct device_node *of_node_get(struct device_node *node)
+{
+	return node;
+}
+static inline void of_node_put(struct device_node *node) { }
+#endif /* !CONFIG_OF_DYNAMIC */
 
 #ifdef CONFIG_OF
 
