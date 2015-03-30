fe7f9a407ac94615ad84f5411620ad2a4525702a
steppnasty <steppnasty@gmail.com>
2015-03-30 09:46:51 -0400

    Revert "ion: Fix warning on buffer destroy"
    This reverts commit 741714aaabfb22ed73d9b43d8bd95d07366fe546.

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -313,13 +313,13 @@ static struct ion_handle *ion_handle_create(struct ion_client *client,
 static void ion_handle_destroy(struct kref *kref)
 {
 	struct ion_handle *handle = container_of(kref, struct ion_handle, ref);
-	struct ion_client *client = handle->client;
-	struct ion_buffer *buffer = handle->buffer;
-
+	/* XXX Can a handle be destroyed while it's map count is non-zero?:
+	   if (handle->map_cnt) unmap
+	 */
+	WARN_ON(handle->kmap_cnt || handle->dmap_cnt || handle->usermap_cnt);
+	ion_buffer_put(handle->buffer);
 	if (!RB_EMPTY_NODE(&handle->node))
-		rb_erase(&handle->node, &client->handles);
-
-	ion_buffer_put(buffer);
+		rb_erase(&handle->node, &handle->client->handles);
 	kfree(handle);
 }
 
