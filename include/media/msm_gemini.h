ef53b55396d3d7f4faadbead535313f978426506
steppnasty <steppnasty@gmail.com>
2015-03-31 09:25:24 -0400

    msm_gemini: merged updates from android-msm-mako-3.4-jb-mr1.1

--- include/media/msm_gemini.h
+++ include/media/msm_gemini.h
@@ -1,36 +1,7 @@
-/* Copyright (c) 2010, Code Aurora Forum. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *     * Neither the name of Code Aurora Forum, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef __LINUX_MSM_GEMINI_H
 #define __LINUX_MSM_GEMINI_H
 
 #include <linux/types.h>
-#include <asm/sizes.h>
 #include <linux/ioctl.h>
 
 #define MSM_GMN_IOCTL_MAGIC 'g'
@@ -108,6 +79,7 @@ struct msm_gemini_buf {
 	uint32_t cbcr_len;
 
 	uint32_t num_of_mcu_rows;
+	uint32_t offset;
 };
 
 #define MSM_GEMINI_HW_CMD_TYPE_READ      0
