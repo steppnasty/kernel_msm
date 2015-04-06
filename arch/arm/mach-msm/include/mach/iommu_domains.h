c09958bd16c0d066b94c65a9c2d8fae014b5c0cb
steppnasty <steppnasty@gmail.com>
2015-04-06 12:02:33 -0400

    iommu_domains: Merged updates from jb_mr1_rb4.19

--- arch/arm/mach-msm/include/mach/iommu_domains.h
+++ arch/arm/mach-msm/include/mach/iommu_domains.h
@@ -27,6 +27,18 @@ enum {
 	GEN_POOL,
 };
 
+struct msm_iova_partition {
+	unsigned long start;
+	unsigned long size;
+};
+
+struct msm_iova_layout {
+	struct msm_iova_partition *partitions;
+	int npartitions;
+	const char *client_name;
+	unsigned int domain_flags;
+	unsigned int is_secure;
+};
 
 #if defined(CONFIG_MSM_IOMMU)
 
@@ -59,6 +71,22 @@ extern void msm_iommu_unmap_extra(struct iommu_domain *domain,
 						unsigned long size,
 						unsigned long page_size);
 
+extern int msm_iommu_map_contig_buffer(phys_addr_t phys,
+				unsigned int domain_no,
+				unsigned int partition_no,
+				unsigned long size,
+				unsigned long align,
+				unsigned long cached,
+				unsigned long *iova_val);
+
+
+extern void msm_iommu_unmap_contig_buffer(unsigned long iova,
+					unsigned int domain_no,
+					unsigned int partition_no,
+					unsigned long size);
+
+extern int msm_register_domain(struct msm_iova_layout *layout);
+extern int msm_unregister_domain(struct iommu_domain *domain);
 #else
 static inline struct iommu_domain
 	*msm_get_iommu_domain(int subsys_id) { return NULL; }
@@ -104,6 +132,36 @@ static inline void msm_iommu_unmap_extra(struct iommu_domain *domain,
 						unsigned long size,
 						unsigned long page_size)
 {
+}
+
+static inline int msm_iommu_map_contig_buffer(phys_addr_t phys,
+				unsigned int domain_no,
+				unsigned int partition_no,
+				unsigned long size,
+				unsigned long align,
+				unsigned long cached,
+				unsigned long *iova_val)
+{
+	*iova_val = phys;
+	return 0;
+}
+
+static inline void msm_iommu_unmap_contig_buffer(unsigned long iova,
+					unsigned int domain_no,
+					unsigned int partition_no,
+					unsigned long size)
+{
+	return;
+}
+
+static inline int msm_register_domain(struct msm_iova_layout *layout)
+{
+	return -ENODEV;
+}
+
+static inline int msm_unregister_domain(struct iommu_domain *domain)
+{
+	return -ENODEV;
 }
 #endif
 
