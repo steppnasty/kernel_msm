d709d29fef5e4396c5373f2a9ecc58fce1823c98
steppnasty <steppnasty@gmail.com>
2017-02-27 13:52:13 -0500

    kgsl: Move platform_devices to devices-msm7x30.c

---
+++ arch/arm/mach-msm/devices-msm7x30.c
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2008 Google, Inc.
+ * Copyright (c) 2008-2010, Code Aurora Forum. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/platform_device.h>
+#include <mach/kgsl.h>
+#include <mach/irqs.h>
+
+#include <mach/dal_axi.h>
+
+static struct resource kgsl_3d0_resources[] = {
+	{
+		.name  = KGSL_3D0_REG_MEMORY,
+		.start = 0xA3500000, /* 3D GRP address */
+		.end = 0xA351ffff,
+		.flags = IORESOURCE_MEM,
+	},
+	{
+		.name = KGSL_3D0_IRQ,
+		.start = INT_GRP_3D,
+		.end = INT_GRP_3D,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+
+static struct kgsl_device_platform_data kgsl_3d0_pdata = {
+	.pwrlevel = {
+		{
+			.gpu_freq = 245760000,
+			.bus_freq = 192000000,
+		},
+		{
+			.gpu_freq = 192000000,
+			.bus_freq = 153000000,
+		},
+		{
+			.gpu_freq = 192000000,
+			.bus_freq = 0,
+		},
+	},
+	.init_level = 0,
+	.num_levels = 3,
+	.set_grp_async = set_grp3d_async,
+	.idle_timeout = HZ/20,
+	.nap_allowed = true,
+	.clk = {
+		.clk = "grp_clk",
+		.pclk = "grp_pclk",
+	},
+	.imem_clk_name = {
+		.clk = "imem_clk",
+		.pclk = NULL,
+	},
+};
+
+struct platform_device msm_kgsl_3d0 = {
+	.name = "kgsl-3d0",
+	.id = 0,
+	.num_resources = ARRAY_SIZE(kgsl_3d0_resources),
+	.resource = kgsl_3d0_resources,
+	.dev = {
+		.platform_data = &kgsl_3d0_pdata,
+	},
+};
+
+static struct resource kgsl_2d0_resources[] = {
+	{
+		.name = KGSL_2D0_REG_MEMORY,
+		.start = 0xA3900000, /* Z180 base address */
+		.end = 0xA3900FFF,
+		.flags = IORESOURCE_MEM,
+	},
+	{
+		.name = KGSL_2D0_IRQ,
+		.start = INT_GRP_2D,
+		.end = INT_GRP_2D,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+
+static struct kgsl_device_platform_data kgsl_2d0_pdata = {
+	.pwrlevel = {
+		{
+			.gpu_freq = 0,
+			.bus_freq = 192000000,
+		},
+	},
+	.init_level = 0,
+	.num_levels = 1,
+	/* HW workaround, run Z180 SYNC @ 192 MHZ */
+	.set_grp_async = NULL,
+	.idle_timeout = HZ/10,
+	.nap_allowed = true,
+	.clk = {
+		.clk = "grp_2d_clk",
+		.pclk = "grp_2d_pclk",
+	},
+};
+
+struct platform_device msm_kgsl_2d0 = {
+	.name = "kgsl-2d0",
+	.id = 0,
+	.num_resources = ARRAY_SIZE(kgsl_2d0_resources),
+	.resource = kgsl_2d0_resources,
+	.dev = {
+		.platform_data = &kgsl_2d0_pdata,
+	},
+};
--- arch/arm/mach-msm/devices_htc.c
+++ arch/arm/mach-msm/devices_htc.c
@@ -19,7 +19,6 @@
 #include <linux/platform_device.h>
 
 #include <linux/dma-mapping.h>
-#include <mach/kgsl.h>
 #include <mach/msm_iomap.h>
 #include <mach/dma.h>
 #include "gpio_chip.h"
@@ -38,8 +37,6 @@
 #include <mach/msm_rpcrouter.h>
 #include <mach/msm_iomap.h>
 #include <asm/mach/mmc.h>
-#include <linux/msm_kgsl.h>
-#include <mach/dal_axi.h>
 #include "proc_comm.h"
 
 static char *df_serialno = "000000000000";
@@ -110,109 +107,6 @@ struct platform_device msm_device_cam = {
 	.name		= "msm",
 	.id		= 0,
 };
-#endif
-
-#ifdef CONFIG_MSM_KGSL
-static struct resource kgsl_3d0_resources[] = {
-	{
-		.name  = KGSL_3D0_REG_MEMORY,
-		.start = 0xA3500000, /* 3D GRP address */
-		.end = 0xA351ffff,
-		.flags = IORESOURCE_MEM,
-	},
-	{
-		.name = KGSL_3D0_IRQ,
-		.start = INT_GRP_3D,
-		.end = INT_GRP_3D,
-		.flags = IORESOURCE_IRQ,
-	},
-};
-
-static struct kgsl_device_platform_data kgsl_3d0_pdata = {
-	.pwrlevel = {
-		{
-			.gpu_freq = 245760000,
-			.bus_freq = 192000000,
-		},
-		{
-			.gpu_freq = 192000000,
-			.bus_freq = 153000000,
-		},
-		{
-			.gpu_freq = 192000000,
-			.bus_freq = 0,
-		},
-	},
-	.init_level = 0,
-	.num_levels = 3,
-	.set_grp_async = set_grp3d_async,
-	.idle_timeout = HZ/20,
-	.nap_allowed = true,
-	.clk = {
-		.clk = "grp_clk",
-		.pclk = "grp_pclk",
-	},
-	.imem_clk_name = {
-		.clk = "imem_clk",
-		.pclk = NULL,
-	},
-};
-
-struct platform_device msm_kgsl_3d0 = {
-	.name = "kgsl-3d0",
-	.id = 0,
-	.num_resources = ARRAY_SIZE(kgsl_3d0_resources),
-	.resource = kgsl_3d0_resources,
-	.dev = {
-		.platform_data = &kgsl_3d0_pdata,
-	},
-};
-
-#ifdef CONFIG_MSM_KGSL_2D
-static struct resource kgsl_2d0_resources[] = {
-	{
-		.name = KGSL_2D0_REG_MEMORY,
-		.start = 0xA3900000, /* Z180 base address */
-		.end = 0xA3900FFF,
-		.flags = IORESOURCE_MEM,
-	},
-	{
-		.name = KGSL_2D0_IRQ,
-		.start = INT_GRP_2D,
-		.end = INT_GRP_2D,
-		.flags = IORESOURCE_IRQ,
-	},
-};
-
-static struct kgsl_device_platform_data kgsl_2d0_pdata = {
-	.pwrlevel = {
-		{
-			.gpu_freq = 0,
-			.bus_freq = 192000000,
-		},
-	},
-	.init_level = 0,
-	.num_levels = 1,
-	/* HW workaround, run Z180 SYNC @ 192 MHZ */
-	.set_grp_async = NULL,
-	.idle_timeout = HZ/10,
-	.nap_allowed = true,
-	.clk = {
-		.clk = "grp_2d_clk",
-		.pclk = "grp_2d_pclk",
-	},
-};
-
-struct platform_device msm_kgsl_2d0 = {
-	.name = "kgsl-2d0",
-	.id = 0,
-	.num_resources = ARRAY_SIZE(kgsl_2d0_resources),
-	.resource = kgsl_2d0_resources,
-	.dev = {
-		.platform_data = &kgsl_2d0_pdata,
-	},
-};
-#endif
 #endif
 
 #define PM_LIBPROG      0x30000061
