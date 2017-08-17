1412ab6885025c9aa8148f4c48a649e8ce753450
steppnasty <steppnasty@gmail.com>
2017-08-17 16:05:13 -0400

    msm: memory.c: Merge in updates from doubleshot-gb-crc-2.6.35

--- arch/arm/mach-msm/memory.c
+++ arch/arm/mach-msm/memory.c
@@ -17,6 +17,7 @@
 #include <linux/mm_types.h>
 #include <linux/bootmem.h>
 #include <linux/memory_alloc.h>
+#include <linux/module.h>
 #include <asm/pgtable.h>
 #include <asm/io.h>
 #include <asm/mach/map.h>
@@ -29,7 +30,7 @@ int arch_io_remap_pfn_range(struct vm_area_struct *vma, unsigned long addr,
 	unsigned long pfn_addr = pfn << PAGE_SHIFT;
 	if ((pfn_addr >= 0x88000000) && (pfn_addr < 0xD0000000)) {
 		prot = pgprot_device(prot);
-		pr_debug("remapping device %lx\n", prot);
+		printk("remapping device %lx\n", prot);
 	}
 	return remap_pfn_range(vma, addr, pfn, size, prot);
 }
@@ -51,6 +52,8 @@ void write_to_strongly_ordered_memory(void)
 	map_zero_page_strongly_ordered();
 	*(int *)zero_page_strongly_ordered = 0;
 }
+EXPORT_SYMBOL(write_to_strongly_ordered_memory);
+
 void flush_axi_bus_buffer(void)
 {
 	__asm__ __volatile__ ("mcr p15, 0, %0, c7, c10, 5" \
@@ -110,6 +113,52 @@ void invalidate_caches(unsigned long vstart,
 	asm ("mcr p15, 0, %0, c7, c5, 0" : : "r" (0));
 
 	flush_axi_bus_buffer();
+}
+
+void *alloc_bootmem_aligned(unsigned long size, unsigned long alignment)
+{
+	void *unused_addr = NULL;
+	unsigned long addr, tmp_size, unused_size;
+
+	/* Allocate maximum size needed, see where it ends up.
+	 * Then free it -- in this path there are no other allocators
+	 * so we can depend on getting the same address back
+	 * when we allocate a smaller piece that is aligned
+	 * at the end (if necessary) and the piece we really want,
+	 * then free the unused first piece.
+	 */
+
+	tmp_size = size + alignment - PAGE_SIZE;
+	addr = (unsigned long)alloc_bootmem(tmp_size);
+	free_bootmem(__pa(addr), tmp_size);
+
+	unused_size = alignment - (addr % alignment);
+	if (unused_size)
+		unused_addr = alloc_bootmem(unused_size);
+
+	addr = (unsigned long)alloc_bootmem(size);
+	if (unused_size)
+		free_bootmem(__pa(unused_addr), unused_size);
+
+	return (void *)addr;
+}
+
+int platform_physical_remove_pages(unsigned long start_pfn,
+	unsigned long nr_pages)
+{
+	return 1;
+}
+
+int platform_physical_active_pages(unsigned long start_pfn,
+	unsigned long nr_pages)
+{
+	return 1;
+}
+
+int platform_physical_low_power_pages(unsigned long start_pfn,
+	unsigned long nr_pages)
+{
+	return 1;
 }
 
 unsigned long allocate_contiguous_ebi_nomap(unsigned long size,
