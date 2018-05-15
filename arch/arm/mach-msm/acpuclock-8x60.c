1a2618f2227cd6fd16de5355909645abab5784bf
steppnasty <steppnasty@gmail.com>
2018-05-15 11:54:17 -0400

    Revert "board/cpufreq: add early suspend/late resume drivers"
    This reverts commit f7941d10304d46a6a2af6c16dc5b86308f50efec.

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -36,8 +36,6 @@
 #define dprintk(msg...) \
 	cpufreq_debug_printk(CPUFREQ_DEBUG_DRIVER, "cpufreq-msm", msg)
 
-extern unsigned int max_capped;
-
 /* Frequency switch modes. */
 #define SHOT_SWITCH		4
 #define HOP_SWITCH		5
@@ -602,9 +600,6 @@ static int acpuclk_8x60_set_rate(int cpu, unsigned long rate,
 		rc = -EINVAL;
 		goto out;
 	}
-
-	if (max_capped && rate > max_capped)
-		rate = max_capped;
 
 	if (reason == SETRATE_CPUFREQ || reason == SETRATE_HOTPLUG)
 		mutex_lock(&drv_state.lock);
