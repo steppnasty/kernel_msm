203a01d97f7c51035e387e223c5635c67c95c8a8
steppnasty <steppnasty@gmail.com>
2019-02-08 13:43:03 -0500

    mmc: Don't subtract boot sectors from disc size.

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -251,12 +251,6 @@ static int mmc_read_ext_csd(struct mmc_card *card)
 			ext_csd[EXT_CSD_SEC_CNT + 1] << 8 |
 			ext_csd[EXT_CSD_SEC_CNT + 2] << 16 |
 			ext_csd[EXT_CSD_SEC_CNT + 3] << 24;
-		if (card->ext_csd.sectors) {
-			unsigned boot_sectors;
-			/* size is in 256K chunks, i.e. 512 sectors each */
-			boot_sectors = ext_csd[EXT_CSD_BOOT_SIZE_MULTI] * 512;
-			card->ext_csd.sectors -= boot_sectors;
-		}
 	}
 
 	switch (ext_csd[EXT_CSD_CARD_TYPE] & EXT_CSD_CARD_TYPE_MASK) {
